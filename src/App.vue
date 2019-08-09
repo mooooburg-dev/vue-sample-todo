7d4e8ca98226ea8d6debe1c416d0f549ad246476
mooooburg <jeongmupark@gmail.com>
2019-08-09 13:14:30 +0900

    상태별 보기 버튼 추가

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,21 @@
   <div id="app">
     <div class="container">
       <div class="col-md-6 offset-md-3">
-        <h1 class="text-center mb-4">Todo 어플리케이션</h1>
+        <h1 class="text-center mb-4">TODO App (Vue.js)</h1>
         <input type="text" class="form-control mb-4" v-model="userInput" @keyup.enter="addNewTodo" />
 
-        <div class="list-group">
-          <template v-for="todo in todoList">
+        <div class="list-group mb-4">
+          <template v-for="todo in activeTodoList">
             <button class="list-group-item text-left" @click="toggleTodoState(todo)">
-              {{ todo }}
+              {{ todo.label }}
             </button>
           </template>
+        </div>
+
+        <div class="text-right">
+          <button type="button" class='btn btn-sm' @click="changeCurrentState('active')">할 일</button>
+          <button type="button" class='btn btn-sm' @click="changeCurrentState('done')">완료</button>
+          <button type="button" class='btn btn-sm' @click="changeCurrentState('all')">전체</button>
         </div>
       </div>
     </div>
@@ -23,10 +29,19 @@ export default {
   data(){
     return {
       userInput: '',
-      todoList: []
+      todoList: [],
+      currentState: 'active'
     };
   },
+  computed: {
+    activeTodoList(){
+      return this.todoList.filter(todo => this.currentState === 'all' || todo.state === this.currentState);
+    }
+  },
   methods: {
+    changeCurrentState(state){
+      this.currentState = state;
+    },
     addNewTodo(){
       this.todoList.push({
         label: this.userInput,
