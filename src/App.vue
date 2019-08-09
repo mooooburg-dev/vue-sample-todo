0aee457d3a3f10330092f681c67f4638361de8c0
mooooburg <jeongmupark@gmail.com>
2019-08-09 13:02:48 +0900

    state 셋팅 추가하기

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,15 @@
     <div class="container">
       <div class="col-md-6 offset-md-3">
         <h1 class="text-center mb-4">Todo 어플리케이션</h1>
-        <input type="text" class="form-control" v-model="userInput" @keyup.enter="addNewTodo" />
+        <input type="text" class="form-control mb-4" v-model="userInput" @keyup.enter="addNewTodo" />
+
+        <div class="list-group">
+          <template v-for="todo in todoList">
+            <button class="list-group-item text-left" @click="toggleTodoState(todo)">
+              {{ todo }}
+            </button>
+          </template>
+        </div>
       </div>
     </div>
   </div>
@@ -20,8 +28,14 @@ export default {
   },
   methods: {
     addNewTodo(){
-      this.todoList.push(this.userInput);
+      this.todoList.push({
+        label: this.userInput,
+        state: 'active'
+      });
       this.userInput = '';
+    },
+    toggleTodoState(todo){
+      todo.state = todo.state === 'active' ? 'done' : 'active';
     }
   },
   components: {
